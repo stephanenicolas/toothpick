ba4bf82529943eca11a441e3b3715262537b211f
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-03-27 21:33:44 -0700

    Create toothpic API + some tests

--- toothpick-runtime/src/main/java/toothpick/ToothPick.java
+++ toothpick-runtime/src/main/java/toothpick/ToothPick.java
@@ -1,8 +1,67 @@
 package toothpick;
 
+import java.util.concurrent.ConcurrentHashMap;
+import toothpick.config.Module;
+
+import static java.lang.String.format;
+
 /**
  * Main class to access toothpick features.
  * It allows to create / retrieve injectors.
  */
-public class ToothPick {
+public final class ToothPick {
+
+  private static ConcurrentHashMap<Object, Injector> mapKeyToInjector = new ConcurrentHashMap<>();
+
+  private ToothPick() {
+  }
+
+  public static Injector createInjector(Object key, Module... modules) {
+    return createInjector(null, key, modules);
+  }
+
+  public static Injector createInjector(Injector parent, Object key, Module... modules) {
+    Injector injector = mapKeyToInjector.get(key);
+    if (injector != null) {
+      throw new IllegalStateException(format("An injector for key %s already exists: %s", key, injector));
+    }
+
+    synchronized (mapKeyToInjector) {
+      injector = mapKeyToInjector.get(key);
+      if (injector != null) {
+        throw new IllegalStateException(format("An injector for key %s already exists: %s", key, injector));
+      }
+      injector = new InjectorImpl(parent, key, modules);
+      mapKeyToInjector.put(key, injector);
+    }
+    return injector;
+  }
+
+  public static Injector getInjector(Object key) {
+    return mapKeyToInjector.get(key);
+  }
+
+  public static Injector getOrCreateInjector(Injector parent, Object key, Module... modules) {
+    Injector injector = getInjector(key);
+    if (injector == null) {
+      synchronized (mapKeyToInjector) {
+        if (injector == null) {
+          injector = createInjector(parent, key, modules);
+        }
+      }
+    }
+    return injector;
+  }
+
+  public static void destroyInjector(Object key) {
+    Injector injector = getInjector(key);
+    if (injector == null) {
+      return;
+    }
+    mapKeyToInjector.remove(key);
+  }
+
+  public static void reset() {
+    mapKeyToInjector.clear();
+  }
 }
---
+++ toothpick-runtime/src/src/test/java/toothpick/ToothPickTest.java
@@ -0,0 +1,95 @@
+package toothpick;
+
+import org.junit.After;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ToothPickTest {
+
+  @Test
+  public void getInjector_shouldReturnNull_whenNoInjectorByThisKeyWasCreated() throws Exception {
+    //GIVEN
+
+    //WHEN
+    Injector injector = ToothPick.getInjector(this);
+
+    //THEN
+    assertThat(injector, nullValue());
+  }
+
+  @Test
+  public void getInjector_shouldReturnAnInjector_whenThisInjectorByThisKeyWasCreated() throws Exception {
+    //GIVEN
+    Injector injector = ToothPick.createInjector(this);
+
+    //WHEN
+    Injector injector2 = ToothPick.getInjector(this);
+
+    //THEN
+    assertThat(injector, notNullValue());
+    assertThat(injector, sameInstance(injector2));
+  }
+
+  @Test
+  public void createInjector_shouldReturnAnInjectorWithAParent_whenThisInjectorByThisKeyWasCreatedWithAParent() throws Exception {
+    //GIVEN
+    Injector injectorParent = ToothPick.createInjector("foo");
+
+    //WHEN
+    Injector injector = ToothPick.createInjector(injectorParent, "bar");
+
+    //THEN
+    assertThat(injector, notNullValue());
+    assertThat(injector.getParent(), sameInstance(injectorParent));
+  }
+
+  @Test
+  public void reset_shouldClear_WhenSomeInjectorsWereCreated() throws Exception {
+    //GIVEN
+    ToothPick.createInjector("foo");
+    ToothPick.createInjector("bar");
+
+    //WHEN
+    ToothPick.reset();
+    Injector injector0AfterReset = ToothPick.getInjector("foo");
+    Injector injector1AfterReset = ToothPick.getInjector("bar");
+
+    //THEN
+    assertThat(injector0AfterReset, nullValue());
+    assertThat(injector1AfterReset, nullValue());
+  }
+
+  @Test
+  public void destroyInjector_shouldClearThisInjector_WhenThisInjectorsWasCreated() throws Exception {
+    //GIVEN
+    ToothPick.createInjector("foo");
+
+    //WHEN
+    ToothPick.destroyInjector("foo");
+    Injector injectorAfterReset = ToothPick.getInjector("foo");
+
+    //THEN
+    assertThat(injectorAfterReset, nullValue());
+  }
+
+  @Test
+  public void destroyInjector_shouldClearThisInjector_WhenThisInjectorsWasNotCreated() throws Exception {
+    //GIVEN
+
+    //WHEN
+    ToothPick.destroyInjector("foo");
+    Injector injectorAfterReset = ToothPick.getInjector("foo");
+
+    //THEN
+    assertThat(injectorAfterReset, nullValue());
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    ToothPick.reset();
+  }
+}
