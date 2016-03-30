2e6ebec17b66730c8ec02d4ca7fc9e4c506ec3d9
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-03-30 08:19:01 -0700

    Use better composition for dealing with inheritance of injection

--- toothpick-runtime/src/test/java/toothpick/integration/data/FooChildWithInjectedFields$$MemberInjector.java
+++ toothpick-runtime/src/test/java/toothpick/integration/data/FooChildWithInjectedFields$$MemberInjector.java
@@ -1,12 +1,15 @@
 package toothpick.integration.data;
 
 import toothpick.Injector;
+import toothpick.MemberInjector;
+import toothpick.MemberInjectorRegistry;
 
-@SuppressWarnings("unused") public class FooChildWithInjectedFields$$MemberInjector
-    extends Foo$$MemberInjector {
-  @Override public void inject(Foo foo, Injector injector) {
-    ((FooChildWithInjectedFields)foo).bar2 = injector.getInstance(Bar.class);
-    super.inject(foo, injector);
+@SuppressWarnings("unused") public class FooChildWithInjectedFields$$MemberInjector implements MemberInjector<FooChildWithInjectedFields> {
+  private MemberInjector<Foo> fooMemberInjector = MemberInjectorRegistry.getMemberInjector(Foo.class);
+
+  @Override public void inject(FooChildWithInjectedFields foo, Injector injector) {
+    foo.bar2 = injector.getInstance(Bar.class);
+    fooMemberInjector.inject(foo, injector);
   }
 }
 
--- toothpick-runtime/src/test/java/toothpick/integration/data/FooGrandChildWithInjectedFields$$MemberInjector.java
+++ toothpick-runtime/src/test/java/toothpick/integration/data/FooGrandChildWithInjectedFields$$MemberInjector.java
@@ -1,13 +1,16 @@
 package toothpick.integration.data;
 
 import toothpick.Injector;
+import toothpick.MemberInjector;
+import toothpick.MemberInjectorRegistry;
 
 //as parent doesn't define inject annotated fields, we jump to grand parent member injector.
-@SuppressWarnings("unused") public class FooGrandChildWithInjectedFields$$MemberInjector
-    extends Foo$$MemberInjector {
-  @Override public void inject(Foo foo, Injector injector) {
-    ((FooGrandChildWithInjectedFields)foo).bar2 = injector.getInstance(Bar.class);
-    super.inject(foo, injector);
+@SuppressWarnings("unused") public class FooGrandChildWithInjectedFields$$MemberInjector implements MemberInjector<FooGrandChildWithInjectedFields> {
+  private MemberInjector<Foo> fooMemberInjector = MemberInjectorRegistry.getMemberInjector(Foo.class);
+
+  @Override public void inject(FooGrandChildWithInjectedFields foo, Injector injector) {
+    foo.bar2 = injector.getInstance(Bar.class);
+    fooMemberInjector.inject(foo, injector);
   }
 }
 
