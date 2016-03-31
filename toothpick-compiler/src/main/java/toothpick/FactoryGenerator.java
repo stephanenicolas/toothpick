17a0c3c7f2099ee99106726da2f387570427fc13
Daniel Molinero Reguera <dmolinero@groupon.com>
2016-03-31 23:58:22 +0100

    Add Factory processor generator

--- toothpick-compiler/src/main/java/toothpick/FactoryGenerator.java
+++ toothpick-compiler/src/main/java/toothpick/FactoryGenerator.java
@@ -1,5 +1,14 @@
 package toothpick;
 
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
+
 public class FactoryGenerator {
 
   private static final String FACTORY_SUFFIX = "$$Factory";
@@ -11,18 +20,51 @@ public class FactoryGenerator {
   }
 
   public String brewJava() {
-    /*TypeSpec.Builder injectorTypeSpec =
-        TypeSpec.classBuilder(injectionTarget.className + FACTORY_SUFFIX)
-            .addModifiers(Modifier.PUBLIC);
-    emitInject(injectorTypeSpec);
-    JavaFile javaFile = JavaFile.builder(target.classPackage, injectorTypeSpec.build())
-        .addFileComment("Generated code from Dart. Do not modify!")
+    // Interface to implement
+    ClassName className = ClassName.get(factoryInjectionTarget.classPackage,
+        factoryInjectionTarget.className);
+    ParameterizedTypeName parameterizedTypeName = ParameterizedTypeName.get(className,
+        ClassName.get(Factory.class));
+
+    // Build class
+    TypeSpec.Builder injectorTypeSpec =
+        TypeSpec.classBuilder(factoryInjectionTarget.className + FACTORY_SUFFIX)
+            .addModifiers(Modifier.PUBLIC)
+            .addSuperinterface(parameterizedTypeName);
+    emitCreateInstance(injectorTypeSpec);
+
+    JavaFile javaFile = JavaFile.builder(factoryInjectionTarget.classPackage, injectorTypeSpec.build())
+        .addFileComment("Generated code from ToothPick. Do not modify!")
         .build();
-    return javaFile.toString();*/
-    return "";
+    return javaFile.toString();
   }
 
   public String getFqcn() {
     return factoryInjectionTarget.getFqcn() + FACTORY_SUFFIX;
+  }
+
+  private void emitCreateInstance(TypeSpec.Builder builder) {
+    MethodSpec.Builder factoryBuilder = MethodSpec.methodBuilder("createInstance")
+        .addModifiers(Modifier.PUBLIC)
+        .addParameter(ClassName.get(Injector.class), "injector")
+        .returns(ClassName.get(factoryInjectionTarget.classPackage,
+            factoryInjectionTarget.className));
+
+    StringBuilder returnStatement = new StringBuilder("return new ");
+    returnStatement.append(factoryInjectionTarget.className).append("(");
+    int counter = 1;
+
+    for (TypeMirror typeMirror : factoryInjectionTarget.parameters) {
+      String paramName = String.format("param$d", counter);
+      TypeName paramType = TypeName.get(typeMirror);
+      factoryBuilder.addStatement("$T $S = injector.getInstance($T.class)", paramType, paramName,
+          paramType);
+      returnStatement.append(paramName);
+    }
+
+    returnStatement.append(")");
+    factoryBuilder.addStatement(returnStatement.toString());
+
+    builder.addMethod(factoryBuilder.build());
   }
 }
