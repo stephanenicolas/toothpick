fbcc02002500efbdf671136f2da38b80ce1cf331
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-08 20:37:40 -0700

    Better separation of integration and unit tests. 1 more unit test

--- toothpick-runtime/src/test/java/toothpick/InjectorImplTest.java
+++ toothpick-runtime/src/test/java/toothpick/InjectorImplTest.java
@@ -1,27 +1,29 @@
 package toothpick;
 
+import javax.inject.Provider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import toothpick.config.Module;
-import toothpick.integration.data.Bar;
+import toothpick.config.Binding;
 import toothpick.integration.data.Foo;
+import toothpick.registries.factory.FactoryRegistryLocator;
 import toothpick.registries.memberinjector.MemberInjectorRegistryLocator;
 
-import static org.powermock.api.easymock.PowerMock.createMock;
 import static org.easymock.EasyMock.expect;
-import static org.powermock.api.easymock.PowerMock.verify;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
+import static org.powermock.api.easymock.PowerMock.createMock;
 import static org.powermock.api.easymock.PowerMock.expectLastCall;
 import static org.powermock.api.easymock.PowerMock.mockStatic;
 import static org.powermock.api.easymock.PowerMock.replay;
+import static org.powermock.api.easymock.PowerMock.verify;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ MemberInjectorRegistryLocator.class })
+@PrepareForTest({ MemberInjectorRegistryLocator.class, FactoryRegistryLocator.class })
 public class InjectorImplTest extends ToothPickBaseTest {
 
   @Test
@@ -57,83 +59,30 @@ public class InjectorImplTest extends ToothPickBaseTest {
     fail("Should not allow null bindings");
   }
 
+  @Test
+  public void toProvider_shoudReturnAProviderThatUsesTheFactoryToCreateInstances_whenBindingIsSimpleAndClassNotSingleton() throws Exception {
+    //GIVEN
+    InjectorImpl injector = new InjectorImpl();
+    Factory mockFactory = createMock(Factory.class);
+    mockStatic(FactoryRegistryLocator.class);
+    expect(FactoryRegistryLocator.getFactory(Foo.class)).andReturn(mockFactory).anyTimes();
+    expect(mockFactory.hasSingletonAnnotation()).andReturn(false);
+    expect(mockFactory.createInstance(injector)).andReturn(new Foo());
+    expect(mockFactory.createInstance(injector)).andReturn(new Foo());
+    replay(FactoryRegistryLocator.class, mockFactory);
+
+    //WHEN
+    Binding<Foo> binding = new Binding<>(Foo.class);
+    Provider<Foo> fooProvider = injector.toProvider(binding);
+    Foo actualFoo = fooProvider.get();
+    Foo actualFoo2 = fooProvider.get();
+
+    //THEN
+    assertThat(actualFoo, notNullValue());
+    assertThat(actualFoo2, notNullValue());
+    assertThat(actualFoo, not(sameInstance(actualFoo2)));
+    verify(FactoryRegistryLocator.class, mockFactory);
+  }
+
   // Modules
-
-  @Test
-  public void installOverrideModules_shoudInstallOverrideBindings_whenCalledOnce() {
-    //GIVEN
-    Foo testFoo = new Foo();
-    InjectorImpl injector = new InjectorImpl(new ProdModule());
-    injector.installOverrideModules(new TestModule(testFoo));
-
-    //WHEN
-    Foo instance = injector.getInstance(Foo.class);
-
-    //THEN
-    assertThat(instance, sameInstance(testFoo));
-  }
-
-  @Test
-  public void installOverrideModules_shoudNotInstallOverrideBindings_whenCalledWithoutTestModules() {
-    //GIVEN
-    InjectorImpl injector = new InjectorImpl(new ProdModule());
-    injector.installOverrideModules();
-
-    //WHEN
-    Foo instance = injector.getInstance(Foo.class);
-
-    //THEN
-    assertThat(instance, notNullValue());
-  }
-
-  @Test
-  public void installOverrideModules_shoudInstallOverrideBindingsAgain_whenCalledTwice() {
-    //GIVEN
-    Foo testFoo = new Foo();
-    Foo testFoo2 = new Foo();
-    InjectorImpl injector = new InjectorImpl(new ProdModule());
-    injector.installOverrideModules(new TestModule(testFoo));
-    injector.installOverrideModules(new TestModule(testFoo2));
-
-    //WHEN
-    Foo instance = injector.getInstance(Foo.class);
-
-    //THEN
-    assertThat(instance, sameInstance(testFoo2));
-  }
-
-  @Test
-  public void installOverrideModules_shoudNotOverrideOtherBindings() {
-    //GIVEN
-    Foo testFoo = new Foo();
-    InjectorImpl injector = new InjectorImpl(new ProdModule2());
-    injector.installOverrideModules(new TestModule(testFoo));
-
-    //WHEN
-    Foo fooInstance = injector.getInstance(Foo.class);
-    Bar barInstance = injector.getInstance(Bar.class);
-
-    //THEN
-    assertThat(fooInstance, sameInstance(testFoo));
-    assertThat(barInstance, notNullValue());
-  }
-
-  private static class TestModule extends Module {
-    public TestModule(Foo foo) {
-      bind(Foo.class).to(foo);
-    }
-  }
-
-  private static class ProdModule extends Module {
-    public ProdModule() {
-      bind(Foo.class).to(new Foo());
-    }
-  }
-
-  private static class ProdModule2 extends Module {
-    public ProdModule2() {
-      bind(Foo.class).to(new Foo());
-      bind(Bar.class).to(new Bar());
-    }
-  }
 }
---
+++ toothpick-runtime/src/test/java/toothpick/integration/InjectorImplTest.java
@@ -0,0 +1,110 @@
+package toothpick.integration;
+
+import org.junit.Test;
+import toothpick.InjectorImpl;
+import toothpick.ToothPickBaseTest;
+import toothpick.config.Module;
+import toothpick.integration.data.Bar;
+import toothpick.integration.data.Foo;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class InjectorImplTest extends ToothPickBaseTest {
+
+  @Test(expected = IllegalStateException.class)
+  public void toProvider_shoudThrowException_whenBindingIsNull() throws Exception {
+    //GIVEN
+    Module module = new Module();
+    module.getBindingSet().add(null);
+    InjectorImpl injector = new InjectorImpl(module);
+
+    //WHEN
+    injector.getInstance(null);
+
+    //THEN
+    fail("Should not allow null bindings");
+  }
+
+  // Modules
+
+  @Test
+  public void installOverrideModules_shoudInstallOverrideBindings_whenCalledOnce() {
+    //GIVEN
+    Foo testFoo = new Foo();
+    InjectorImpl injector = new InjectorImpl(new ProdModule());
+    injector.installOverrideModules(new TestModule(testFoo));
+
+    //WHEN
+    Foo instance = injector.getInstance(Foo.class);
+
+    //THEN
+    assertThat(instance, sameInstance(testFoo));
+  }
+
+  @Test
+  public void installOverrideModules_shoudNotInstallOverrideBindings_whenCalledWithoutTestModules() {
+    //GIVEN
+    InjectorImpl injector = new InjectorImpl(new ProdModule());
+    injector.installOverrideModules();
+
+    //WHEN
+    Foo instance = injector.getInstance(Foo.class);
+
+    //THEN
+    assertThat(instance, notNullValue());
+  }
+
+  @Test
+  public void installOverrideModules_shoudInstallOverrideBindingsAgain_whenCalledTwice() {
+    //GIVEN
+    Foo testFoo = new Foo();
+    Foo testFoo2 = new Foo();
+    InjectorImpl injector = new InjectorImpl(new ProdModule());
+    injector.installOverrideModules(new TestModule(testFoo));
+    injector.installOverrideModules(new TestModule(testFoo2));
+
+    //WHEN
+    Foo instance = injector.getInstance(Foo.class);
+
+    //THEN
+    assertThat(instance, sameInstance(testFoo2));
+  }
+
+  @Test
+  public void installOverrideModules_shoudNotOverrideOtherBindings() {
+    //GIVEN
+    Foo testFoo = new Foo();
+    InjectorImpl injector = new InjectorImpl(new ProdModule2());
+    injector.installOverrideModules(new TestModule(testFoo));
+
+    //WHEN
+    Foo fooInstance = injector.getInstance(Foo.class);
+    Bar barInstance = injector.getInstance(Bar.class);
+
+    //THEN
+    assertThat(fooInstance, sameInstance(testFoo));
+    assertThat(barInstance, notNullValue());
+  }
+
+  private static class TestModule extends Module {
+    public TestModule(Foo foo) {
+      bind(Foo.class).to(foo);
+    }
+  }
+
+  private static class ProdModule extends Module {
+    public ProdModule() {
+      bind(Foo.class).to(new Foo());
+    }
+  }
+
+  private static class ProdModule2 extends Module {
+    public ProdModule2() {
+      bind(Foo.class).to(new Foo());
+      bind(Bar.class).to(new Bar());
+    }
+  }
+}
