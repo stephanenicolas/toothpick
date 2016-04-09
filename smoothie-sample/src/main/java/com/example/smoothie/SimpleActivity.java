f022ea90fb545aa8ac5e6f1edf156b7eb726cf20
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-09 08:51:55 -0700

    Add sample to smoothie

---
+++ smoothie-sample/src/main/java/com/example/smoothie/SimpleActivity.java
@@ -0,0 +1,13 @@
+package com.example.smoothie;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.simple_activity);
+  }
+}
---
+++ smoothie-sample/src/main/java/com/example/smoothie/SimpleApp.java
@@ -0,0 +1,10 @@
+package com.example.smoothie;
+
+import android.app.Application;
+
+public class SimpleApp extends Application {
+  @Override
+  public void onCreate() {
+    super.onCreate();
+  }
+}
