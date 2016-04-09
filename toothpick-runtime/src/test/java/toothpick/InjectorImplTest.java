6e16e4e295660ceec787f0594d19ab28f798de7c
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-08 20:49:36 -0700

    more unit testing

--- toothpick-runtime/src/test/java/toothpick/InjectorImplTest.java
+++ toothpick-runtime/src/test/java/toothpick/InjectorImplTest.java
@@ -1,12 +1,14 @@
 package toothpick;
 
 import javax.inject.Provider;
+import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import toothpick.config.Binding;
 import toothpick.integration.data.Foo;
+import toothpick.integration.data.FooSingleton;
 import toothpick.registries.factory.FactoryRegistryLocator;
 import toothpick.registries.memberinjector.MemberInjectorRegistryLocator;
 
@@ -27,7 +29,7 @@ import static org.powermock.api.easymock.PowerMock.verify;
 public class InjectorImplTest extends ToothPickBaseTest {
 
   @Test
-  public void inject_shoudInjectObjectUsingMemberInjector() throws Exception {
+  public void inject_shouldInjectObjectUsingMemberInjector() throws Exception {
     //GIVEN
     Foo foo = new Foo();
     InjectorImpl injector = new InjectorImpl();
@@ -48,7 +50,7 @@ public class InjectorImplTest extends ToothPickBaseTest {
   }
 
   @Test(expected = IllegalStateException.class)
-  public void toProvider_shoudThrowException_whenBindingIsNull() throws Exception {
+  public void toProvider_shouldThrowException_whenBindingIsNull() throws Exception {
     //GIVEN
     InjectorImpl injector = new InjectorImpl();
 
@@ -60,7 +62,7 @@ public class InjectorImplTest extends ToothPickBaseTest {
   }
 
   @Test
-  public void toProvider_shoudReturnAProviderThatUsesTheFactoryToCreateInstances_whenBindingIsSimpleAndClassNotSingleton() throws Exception {
+  public void toProvider_shouldReturnAProviderThatUsesTheFactoryToCreateInstances_whenBindingIsSimpleAndClassNotSingleton() throws Exception {
     //GIVEN
     InjectorImpl injector = new InjectorImpl();
     Factory mockFactory = createMock(Factory.class);
@@ -75,12 +77,41 @@ public class InjectorImplTest extends ToothPickBaseTest {
     Binding<Foo> binding = new Binding<>(Foo.class);
     Provider<Foo> fooProvider = injector.toProvider(binding);
     Foo actualFoo = fooProvider.get();
+    fooProvider = injector.getProvider(Foo.class);
     Foo actualFoo2 = fooProvider.get();
 
     //THEN
     assertThat(actualFoo, notNullValue());
     assertThat(actualFoo2, notNullValue());
     assertThat(actualFoo, not(sameInstance(actualFoo2)));
+    verify(FactoryRegistryLocator.class, mockFactory);
+  }
+
+  @Test
+  public void toProvider_shouldReturnAProviderThatUsesTheFactoryToCreateASingleton_whenBindingIsSimpleAndClassIsSingleton() throws Exception {
+    //GIVEN
+    InjectorImpl injector = new InjectorImpl();
+    Factory mockFactory = createMock(Factory.class);
+    mockStatic(FactoryRegistryLocator.class);
+    expect(FactoryRegistryLocator.getFactory(FooSingleton.class)).andReturn(mockFactory).anyTimes();
+    expect(mockFactory.hasSingletonAnnotation()).andReturn(true);
+    expect(mockFactory.createInstance(injector)).andReturn(new FooSingleton());
+    expect(mockFactory.createInstance(injector)).andThrow(new RuntimeException("Should not have been called twice"));
+    EasyMock.expectLastCall().anyTimes();
+    replay(FactoryRegistryLocator.class, mockFactory);
+
+    //WHEN
+    Binding<FooSingleton> binding = new Binding<>(FooSingleton.class);
+    Provider<FooSingleton> fooProvider = injector.toProvider(binding);
+    FooSingleton actualFoo = fooProvider.get();
+    //we must ask a provider as there is no contract in TP for provider not to change after they trigger...
+    fooProvider = injector.getProvider(FooSingleton.class);
+    FooSingleton actualFoo2 = fooProvider.get();
+
+    //THEN
+    assertThat(actualFoo, notNullValue());
+    assertThat(actualFoo2, notNullValue());
+    assertThat(actualFoo, sameInstance(actualFoo2));
     verify(FactoryRegistryLocator.class, mockFactory);
   }
 
