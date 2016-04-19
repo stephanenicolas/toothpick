ff456179fb75b65eb12f4c33ad4efc1d9ec8ea37
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-19 08:37:18 -0700

    Layout inflater was missing.

--- smoothie/src/main/java/toothpick/smoothie/module/ActivityModule.java
+++ smoothie/src/main/java/toothpick/smoothie/module/ActivityModule.java
@@ -3,6 +3,7 @@ package toothpick.smoothie.module;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.LoaderManager;
+import android.view.LayoutInflater;
 import toothpick.config.Module;
 import toothpick.smoothie.provider.FragmentManagerProvider;
 import toothpick.smoothie.provider.LoaderManagerProvider;
@@ -12,5 +13,6 @@ public class ActivityModule extends Module {
     bind(Activity.class).to(activity);
     bind(FragmentManager.class).toProvider(FragmentManagerProvider.class);
     bind(LoaderManager.class).toProvider(LoaderManagerProvider.class);
+    bind(LayoutInflater.class).to(LayoutInflater.from(activity));
   }
 }
