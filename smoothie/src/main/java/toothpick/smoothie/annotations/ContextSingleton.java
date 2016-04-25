05b100d3a9b11da5397908ad3fc3010634b184ca
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-25 10:43:07 -0700

    Add ContestScope to smoothie

---
+++ smoothie/src/main/java/toothpick/smoothie/annotations/ContextSingleton.java
@@ -0,0 +1,20 @@
+package toothpick.smoothie.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import javax.inject.Scope;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Identifies a type that the injector only instantiates once
+ * per context. This scope annotation has to be bound to
+ * service &amp; activity scopes. Not inherited.
+ *
+ * @see javax.inject.Scope @Scope
+ */
+@Scope
+@Documented
+@Retention(RUNTIME)
+public @interface ContextSingleton {
+}
