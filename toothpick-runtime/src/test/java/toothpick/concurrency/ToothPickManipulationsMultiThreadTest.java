b096b2375f079f8b793e4c960ee30912b6cef256
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-04-30 14:09:38 -0700

    Use reentrant locks, more fine grained lock. Tested that locks can be different. Benchmarked and is far better. 35 ms in some cases to close with synchronized, with locks only max 1 ms.

--- toothpick-runtime/src/test/java/toothpick/concurrency/ToothPickManipulationsMultiThreadTest.java
+++ toothpick-runtime/src/test/java/toothpick/concurrency/ToothPickManipulationsMultiThreadTest.java
@@ -8,12 +8,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import toothpick.ToothPick;
-import toothpick.concurrency.threads.AddNodeThread;
+import toothpick.concurrency.threads.AddSameScopeThread;
 import toothpick.concurrency.threads.AddScopeToListThread;
-import toothpick.concurrency.threads.RemoveNodeThread;
+import toothpick.concurrency.threads.RemoveSameScopeThread;
 import toothpick.concurrency.threads.RemoveScopeFromListThread;
 import toothpick.concurrency.threads.TestableThread;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static toothpick.concurrency.utils.ThreadTestUtil.STANDARD_THREAD_COUNT;
 
@@ -34,7 +37,7 @@ public class ToothPickManipulationsMultiThreadTest {
   }
 
   @Test
-  public void concurrentScopeAdditions_shouldNotCrash() throws InterruptedException {
+  public void concurrentOpenScopes_shouldNotCrash() throws InterruptedException {
     //GIVEN
     final int addNodeThreadCount = STANDARD_THREAD_COUNT;
     List<TestableThread> threadList = new ArrayList<>();
@@ -55,7 +58,7 @@ public class ToothPickManipulationsMultiThreadTest {
   }
 
   @Test
-  public void concurrentScopeRemovals_shouldNotCrash() throws InterruptedException {
+  public void concurrentCloseScopes_shouldNotCrash() throws InterruptedException {
     //GIVEN
     final int scopeCount = 100;
     for (int indexScope = 0; indexScope < scopeCount; indexScope++) {
@@ -82,7 +85,7 @@ public class ToothPickManipulationsMultiThreadTest {
   }
 
   @Test
-  public void concurrentScopeAdditionsAndRemovals_shouldNotCrash() throws InterruptedException {
+  public void concurrentOpenAndCloseScopes_shouldNotCrash() throws InterruptedException {
     //GIVEN
     final int removalNodeThreadCount = STANDARD_THREAD_COUNT;
     final int addNodeThreadCount = STANDARD_THREAD_COUNT;
@@ -107,5 +110,54 @@ public class ToothPickManipulationsMultiThreadTest {
       thread.join();
       assertTrue(String.format("test of thread %s failed", thread.getName()), thread.isSuccessful());
     }
+  }
+
+  @Test
+  public void concurrentOpenScopes_shouldAddChildScopeOnlyOnce_withSameChildScope() throws InterruptedException {
+    //GIVEN
+    final int addSameScopeThreadCount = STANDARD_THREAD_COUNT;
+    List<TestableThread> threadList = new ArrayList<>();
+
+    //WHEN
+    for (int indexThread = 0; indexThread < addSameScopeThreadCount; indexThread++) {
+      final TestableThread testableThread = new AddSameScopeThread(ROOT_SCOPE, "childScope");
+      threadList.add(testableThread);
+      testableThread.start();
+    }
+
+    //THEN
+    for (TestableThread thread : threadList) {
+      thread.join();
+      assertTrue(String.format("test of thread %s failed", thread.getName()), thread.isSuccessful());
+    }
+    assertThat(ToothPick.getScopeNames().size(), is(2));
+  }
+
+  @Test
+  public void concurrentOpenScopes_shouldAddChildScopeAtMostOnce_withSameChildScope() throws InterruptedException {
+    //GIVEN
+    final int addSameScopeThreadCount = STANDARD_THREAD_COUNT;
+    final int removeSameScopeThreadCount = STANDARD_THREAD_COUNT;
+    List<TestableThread> threadList = new ArrayList<>();
+    final Random random = new Random();
+
+    //WHEN
+    for (int indexThread = 0; indexThread < addSameScopeThreadCount + removeSameScopeThreadCount; indexThread++) {
+      final TestableThread testableThread;
+      if (random.nextInt(100) < 50) {
+        testableThread = new RemoveSameScopeThread("childScope");
+      } else {
+        testableThread = new AddSameScopeThread(ROOT_SCOPE, "childScope");
+      }
+      threadList.add(testableThread);
+      testableThread.start();
+    }
+
+    //THEN
+    for (TestableThread thread : threadList) {
+      thread.join();
+      assertTrue(String.format("test of thread %s failed", thread.getName()), thread.isSuccessful());
+    }
+    assertThat(ToothPick.getScopeNames().size(), anyOf(is(1), is(2)));
   }
 }
