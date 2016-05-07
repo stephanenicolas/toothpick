da51e89cfa5eca354919c807816d0c447a8daa4d
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-05-06 18:21:39 -0700

    fix same name children scope in tests

--- toothpick-runtime/src/test/java/toothpick/scoping/ScopingTest.java
+++ toothpick-runtime/src/test/java/toothpick/scoping/ScopingTest.java
@@ -183,8 +183,8 @@ public class ScopingTest extends ToothPickBaseTest {
     //GIVEN
     ToothPick.setConfiguration(development());
     Scope scopeParent = new ScopeImpl("");
-    Scope scope1 = new ScopeImpl("");
-    Scope scope2 = new ScopeImpl("");
+    Scope scope1 = new ScopeImpl("scope1");
+    Scope scope2 = new ScopeImpl("scope2");
     scopeParent.addChild(scope1);
     scopeParent.addChild(scope2);
 
@@ -204,8 +204,8 @@ public class ScopingTest extends ToothPickBaseTest {
     //GIVEN
     ToothPick.setConfiguration(development());
     Scope scopeParent = new ScopeImpl("");
-    Scope scope1 = new ScopeImpl("");
-    Scope scope2 = new ScopeImpl("");
+    Scope scope1 = new ScopeImpl("scope1");
+    Scope scope2 = new ScopeImpl("scope2");
     scopeParent.addChild(scope1);
     scopeParent.addChild(scope2);
 
