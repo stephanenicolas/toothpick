2327bf06ea7d78c99037d2969fc401061a674812
Stéphane Nicolas <steff.nicolas@gmail.com>
2016-05-10 18:12:07 -0700

    test NoFactoryFoundException

--- toothpick-runtime/src/main/java/toothpick/registries/NoFactoryFoundException.java
+++ toothpick-runtime/src/main/java/toothpick/registries/NoFactoryFoundException.java
@@ -4,8 +4,13 @@ import static java.lang.String.format;
 
 public class NoFactoryFoundException extends RuntimeException {
   public NoFactoryFoundException(Class clazz) {
+    this(clazz, null);
+  }
+
+  public NoFactoryFoundException(Class clazz, Throwable cause) {
     super(format("No factory could be found for class %s." //
         + " Check that registries are properly setup with annotation processor arguments, " //
-        + "or use annotations correctly in this class.", clazz.getName()));
+        + "or use annotations correctly in this class.", clazz.getName()), cause);
   }
+
 }
