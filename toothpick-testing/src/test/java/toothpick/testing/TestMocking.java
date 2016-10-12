166383273d09f3e96ff488ba1b62fb54b86fe117
Daniel Molinero Reguera <dmolinero@groupon.com>
2016-10-12 13:28:29 +0100

    Issue 165: Updating @Named to have RUNTIME retention so it can be detected by ToothPickTestModule

--- toothpick-testing/src/test/java/toothpick/testing/TestMocking.java
+++ toothpick-testing/src/test/java/toothpick/testing/TestMocking.java
@@ -1,6 +1,7 @@
 package toothpick.testing;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import org.easymock.EasyMockRule;
 import org.easymock.Mock;
 import org.junit.After;
@@ -23,6 +24,7 @@ public class TestMocking {
 
   EntryPoint entryPoint;
   @Mock Dependency dependency;
+  @Mock @Named("named") Dependency namedDependency;
 
   @After
   public void tearDown() throws Exception {
@@ -46,8 +48,24 @@ public class TestMocking {
     assertThat(num, is(2));
   }
 
+  @Test
+  public void testNamedMock() throws Exception {
+    //GIVEN
+    expect(namedDependency.num()).andReturn(3);
+    replay(namedDependency);
+    //WHEN
+    entryPoint = toothPickRule.getInstance(EntryPoint.class);
+    int num = entryPoint.namedDependency.num();
+    //THEN
+    verify(namedDependency);
+    assertThat(entryPoint, notNullValue());
+    assertThat(entryPoint.namedDependency, notNullValue());
+    assertThat(num, is(3));
+  }
+
   public static class EntryPoint {
     @Inject Dependency dependency;
+    @Inject @Named("named") Dependency namedDependency;
   }
 
   public static class Dependency {
