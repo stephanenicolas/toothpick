69d53faf46bc2e40ccc61645dd65dbc56d00d821
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-08-22 12:37:55 -0700

    address daniel's comment

--- smoothie/src/main/java/toothpick/smoothie/annotations/ContextSingleton.java
+++ smoothie/src/main/java/toothpick/smoothie/annotations/ContextSingleton.java
@@ -12,10 +12,18 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
  * per context. This scope annotation has to be bound to
  * service &amp; activity scopes. Not inherited.
  *
+ * Note that this annotation is just provided to ease the migration
+ * from RG to TP. We highly recommend not keeping it after the migration
+ * is complete and to clearly identify what you inject: either the application context
+ * or an activity (or a service). Using this scope annotation and injection contexts blindly
+ * is a perfect recipe for memory leaks.
+ *
  * @see javax.inject.Scope @Scope
+ * Deprecated, do not inject contexts blindly, this is a bad practice. Read above.
  */
 @Scope
 @Documented
 @Retention(RUNTIME)
+@Deprecated
 public @interface ContextSingleton {
 }
