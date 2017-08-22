00f6fdc59824970aaacc3d45c98a4ad2cf94298d
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-08-22 12:38:28 -0700

    fix CI

--- smoothie/src/main/java/toothpick/smoothie/annotations/ContextSingleton.java
+++ smoothie/src/main/java/toothpick/smoothie/annotations/ContextSingleton.java
@@ -4,7 +4,6 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import javax.inject.Scope;
 
-import static java.lang.annotation.RetentionPolicy.CLASS;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
