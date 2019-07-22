60f7711287de2914eabee152d51d70853cb3df7d
Stéphane Nicolas <steff.nicolas@gmail.com>
2019-07-22 11:58:55 -0700

    more docs

--- smoothie-lifecycle-viewmodel/src/main/kotlin/toothpick/smoothie/viewmodel/ViewModelUtilExtensions.kt
+++ smoothie-lifecycle-viewmodel/src/main/kotlin/toothpick/smoothie/viewmodel/ViewModelUtilExtensions.kt
@@ -7,16 +7,35 @@ import androidx.lifecycle.ViewModelProvider.Factory
 import toothpick.Scope
 import toothpick.config.Module
 
+/**
+ * Closes a scope automatically when the view model of a {@link FragmentActivity} is cleared.
+ *
+ * @param activity the fragment activity to observe.
+ */
 fun Scope.closeOnViewModelCleared(activity: FragmentActivity): Scope {
     ViewModelUtil.closeOnViewModelCleared(activity, this)
     return this
 }
 
+/**
+ * Closes a scope automatically when the view model of a {@link Fragment} is cleared.
+ *
+ * @param fragment the fragment to observe.
+ */
 fun Scope.closeOnViewModelCleared(fragment: Fragment): Scope {
     ViewModelUtil.closeOnViewModelCleared(fragment, this)
     return this
 }
 
+/**
+ * Installs a binding for a view model class.
+ * It will become available by injection. Beware that such a binding
+ * should be installed on a scope that is independent of the activity life cycle.
+ *
+ * @param activity the fragment activity to observe.
+ * @param factory optional factory needed to create the view model instances.
+ * @param viewModelClass the class of the view model to inject.
+ */
 fun <T> Scope.installViewModelBinding(activity: FragmentActivity, factory: Factory? = null, viewModelClass: Class<T>) : Scope where T: ViewModel {
     installModules(object : Module() {
         init {
@@ -27,6 +46,15 @@ fun <T> Scope.installViewModelBinding(activity: FragmentActivity, factory: Facto
     return this
 }
 
+/**
+ * Installs a binding for a view model class.
+ * It will become available by injection. Beware that such a binding
+ * should be installed on a scope that is independent of the fragment life cycle.
+ *
+ * @param fragment the fragment to observe.
+ * @param factory optional factory needed to create the view model instances.
+ * @param viewModelClass the class of the view model to inject.
+ */
 fun <T> Scope.installViewModelBinding(fragment: Fragment, factory: Factory?  = null, viewModelClass: Class<T>) : Scope where T: ViewModel {
     installModules(object : Module() {
         init {
--- smoothie-lifecycle/src/main/kotlin/toothpick/smoothie/lifecyce/LifecycleUtilExtensions.kt
+++ smoothie-lifecycle/src/main/kotlin/toothpick/smoothie/lifecyce/LifecycleUtilExtensions.kt
@@ -3,7 +3,12 @@ package toothpick.smoothie.lifecycle
 import androidx.fragment.app.FragmentActivity
 import toothpick.Scope
 
-fun Scope.closeOnDestroy(activity: FragmentActivity): Scope {
-    LifecycleUtil.closeOnDestroy(activity, this)
+/**
+ * The {@code scope} will be closed automatically during {@code owner}'s {@code onDestroy} event.
+ *
+ * @param owner the lifecycle owner to observe.
+ */
+fun Scope.closeOnDestroy(owner: FragmentActivity): Scope {
+    LifecycleUtil.closeOnDestroy(owner, this)
     return this
 }
